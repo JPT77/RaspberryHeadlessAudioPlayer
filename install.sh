51729600073273b26be71940b2b15d433cff1abb
JPT <j-p-t@gmx.net>
2021-03-11 22:19:59 +0100

    first working

--- install.sh
+++ install.sh
@@ -1,55 +1,68 @@
-#!/bin/sh
+#!/bin/bash
 
-is_installed(){ 
-	# a package having config files still installed will show as installed. Purge first.
-	[ ! dpkg -s $pkgs >/dev/null 2>&1 ]
+function must_install(){ 
+	return "$(apt -qq list $var --installed 2> /dev/null |wc -l)"  # not for scripts
+#	[ $(dpkg -s $var >/dev/null 2>&1) ] # wrong result when not purged
 }
 
-install_if(){ 
-	$install = ""
+function pause() {
+	echo $@
+	read foo
+}
+
+function message() {
+	echo \#\#\# $@
+}
+
+function work() {
+	echo DEBUG $@
+}
+
+function install_if() { 
+	unset install
 	for var in "$@"
 	do
-		if [ ! is_installed($var) ];
+		if $(must_install $var)
 		then
-			$install+="${var} "
-			echo -n .
+			install+="${var} "
+#			echo -n I # install
+# 		else
+#			echo -n S # skip
 		fi
 	done
 	if [ -n "$install" ];
-		sudo apt-get install -qy $install
-		echo
+	then 
+#		echo
+		work sudo apt-get install -qy $install
 	fi
 }
+
+# exit on error
+set -e
 
 echo This script installs a systemd service and several debian and python packages. 
 echo It is going to ask for root password if you didn\'t execute it with root privileges already.
 echo
-echo Press Enter to continue or Ctrl-C to abort.
-read foo
+pause Press Enter to continue or Ctrl-C to abort.
 
-if [ systemctl is-active --quiet sshd ] 
+if [ $(systemctl is-active --quiet sshd) ];
 then
-	echo *** SSH Server already running, skipped.
+	message SSH Server already running, skipped.
 else 
-	echo *** Install SSH Server 
-	install_if(openssh-server)
-	echo *** Enable SSH Server 
-	sudo systemctl enable ssh
-	sudo systemctl start ssh
+	message Install SSH Server 
+	install_if openssh-server
+	message Enable SSH Server 
+	work sudo systemctl enable ssh
+	work sudo systemctl start ssh
 fi
 
-read foo
-if is_installed(network-manager-gnome); 
-else 
-  echo *** Install Wifi requirements
-  sudo apt-get -q install network-manager-gnome
-fi
+message Install Wifi requirements
+install_if network-manager-gnome
 
-read foo
-
-echo *** if you want to login from network to continue the install script, 
-echo *** press Ctrl-C now, set up Wifi if needed 
-echo *** then do your remote login and restart the script
+message If you want to login from network to continue the install script, 
+message press Ctrl-C now, set up Wifi if needed 
+message then do your remote login and restart the script.
+pause Press Enter to continue...
 
 #/etc/hostname setzen, anschliessend Reboot?
 
@@ -57,38 +70,35 @@ echo *** then do your remote login and restart the script
 
 #/etc/systemd/timesyncd.conf
 
-echo *** System update. This may take an hour.
-sudo apt-get -q update
-sudo apt-get -qy upgrade
+message System update. This may take an hour or more.
+work sudo apt-get -q update
+work sudo apt-get -qy upgrade
 
-read foo
+echo \#\#\# Install Media Player Daemon and Command Line Client
+install_if mpd mpc
 
-echo *** Install Media Player Deamon and Command Line Client
-sudo apt-get -q install mpd mpc
-
-read foo
-
-echo *** Install libraries
-sudo apt-get -q install git libespeak1 python3-pip 
+echo \#\#\# Install Libraries
+install_if git libespeak1 python3-pip
+#sudo apt-get -q install git libespeak1 python3-pip 
 
 # TODO Pakete für manuelle Audiosteuerung???
 
 # optional command line tools for GPIO
-sudo apt-get -q install gpio-utils
+install_if gpio-utils
 
-# next may be installed via pip3 too:
-sudo apt-get -q install python3-mpd python3-gst-1.0 python3-rpi.gpio
+# next may be installed via pip3 instead, if libraries are too old.
+install_if python3-mpd python3-gst-1.0 python3-rpi.gpio
 #pip3 install python3-mpd? python3-gst-1.0? python3-rpi.gpio? ???
 
 # must be installed via pip3, no apt-get -q package available
-pip3 install pyttsx3
+work pip3 install pyttsx3
 
-echo *** Install our player script as daemon
-sudo cp playerbuttonmonitor.service /etc/sytemd/system
-sudo chmod 644 /etc/sytemd/system/playerbuttonmonitor.service
-sudo chown root:root /etc/sytemd/system/playerbuttonmonitor.service
-sudo systemctl enable playerbuttonmonitor.service
-sudo systemctl start playerbuttonmonitor.service
+echo \#\#\# Install our player script as daemon
+work sudo cp playerbuttonmonitor.service /etc/sytemd/system
+work sudo chmod 644 		/etc/sytemd/system/playerbuttonmonitor.service
+work sudo chown root:root 	/etc/sytemd/system/playerbuttonmonitor.service
+work sudo systemctl enable 	playerbuttonmonitor.service
+work sudo systemctl start 	playerbuttonmonitor.service
 
 
 
